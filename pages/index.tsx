81d8c49fa602f69fb272f9ff38accf0eb5f787a4
Ayodeji <drealayodeji@icloud.com>
2022-06-11 14:49:33 +0100

    Update: tailwind with postcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,10 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
       <Meta title="Home" desc="This is a description" />
       Hello World!
+      <h1 className="text-6xl my-5">Tailwind working</h1>
       <Dummy />
     </div>
   );
