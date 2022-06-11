4fcd5007d5c4fe41b3e47ec7580a754f7b10348f
Ayodeji <drealayodeji@icloud.com>
2022-06-11 14:52:39 +0100

    Fix: typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <Meta title="Home" desc="This is a description" />
       Hello World!
-      <h1 className="text-6xl my-5">Tailwind working</h1>
+      <h1 className="text-6xl my-5">Tailwind is working</h1>
       <Dummy />
     </div>
   );
